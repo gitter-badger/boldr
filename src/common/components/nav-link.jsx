d9cf6b0da658d5dc35fa0b234c9b576edf8f999a
Steven Truesdell <steven@strues.io>
2016-04-28 21:50:51 -0600

    adds travis

--- src/common/components/nav-link.jsx
+++ src/common/components/nav-link.jsx
@@ -3,21 +3,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class NavLink extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render () {
-        return (
-            <Link
-                {...this.props}
-                activeClassName="active"
-                style={{
-                    textDecoration: 'none'
-                }}
-            />
-        );
-    }
+  render() {
+    return (
+          <Link
+              {...this.props}
+              activeClassName="active"
+              style={{
+                textDecoration: 'none'
+              }}
+          />
+      );
+  }
 }
 
 export default NavLink;
