6718002a153f5510a3312becd792791aa5b82d3e
Steven Truesdell <steven@strues.io>
2016-05-08 13:52:55 -0600

    fix testing for the api

--- compiler.js
+++ compiler.js
@@ -1,8 +1,8 @@
 require('babel-core/register');
 require('jsdom-global')();
-
+const config = require('./tools/config');
 global.nodeRequire = require;
-
+global.__DEV__ = config.__DEV__;
 const noop = (module, file) => {
   module._compile('', file);
 };
