e0a3cd7ae38061c91f620e72712e089591fdd983
Steven Truesdell <steven@strues.io>
2016-05-12 22:10:17 -0600

    async setup of database, tables and indexes.

--- tools/scripts/setupDB.js
+++ tools/scripts/setupDB.js
@@ -1,101 +1,63 @@
 /* eslint-disable no-console */
+import async, { eachSeries } from 'async';
 import r from 'server/db';
 import config from 'config';
 
-function initDB() {
-  r.dbCreate(config.RDB_DB).run();
-}
+const setupDB = () => {
+  async.series([
+    function initDB(callback) {
+      r.dbCreate(config.RDB_DB).run();
+      callback();
+    },
+    function initUser(callback) {
+      r.tableCreate('users').run();
+      callback();
+    },
 
-function initUser() {
-  r.tableCreate('users').run().then(() => buildUserIndexes());
-}
-function buildUserIndexes() {
-  r.table('users').indexCreate('email').run();
-  r.table('users').indexCreate('username').run();
-  r.table('users').indexCreate('createdAt').run();
-}
+    function initRoles(callback) {
+      r.tableCreate('roles').run();
+      callback();
+    },
 
-function initArticle() {
-  r.tableCreate('articles').run()
-  .then(() => buildArticleIndexes());
-}
+    function initArticle(callback) {
+      r.tableCreate('articles').run();
+      callback();
+    },
 
-function buildArticleIndexes() {
-  r.table('articles').indexCreate('title').run();
-  r.table('articles').indexCreate('slug').run();
-  r.table('articles').indexCreate('authorId').run();
-  r.table('articles').indexCreate('isDraft').run();
-  r.table('articles').indexCreate('createdAt').run();
-  r.table('articles').indexCreate('updatedAt').run();
-  r.table('articles').indexCreate('tags', { multi: true }).run();
-  buildArticleRelations();
-}
+    function initTag(callback) {
+      r.tableCreate('tags').run();
+      callback();
+    },
 
-function buildArticleRelations() {
-  r.table('articles').eqJoin('authorId', r.table('users')).run();
-}
+    function initArticleTags(callback) {
+      r.tableCreate('articles_tags').run();
+      callback();
+    },
 
-function initTag() {
-  r.tableCreate('tags').run().then(() => buildTagIndexes());
-}
 
-function buildTagIndexes() {
-  r.table('tags').indexCreate('name').run();
-}
+    function initMenus(callback) {
+      r.tableCreate('menus').run();
+      callback();
+    },
 
-function initArticleTags() {
-  r.tableCreate('articles_tags').run();
-}
+    function initSettings(callback) {
+      r.tableCreate('settings').run();
+      callback();
+    },
+    function initPages(callback) {
+      r.tableCreate('pages').run();
+      callback();
+    },
 
-function initMenus() {
-  r.tableCreate('menus').run()
-  .then(() => buildMenusIndexes());
-}
+    function initCollections(callback) {
+      r.tableCreate('collections').run();
+      callback();
+    },
+    function exit_node(callback) {
+      process.exit();
+      callback();
+    }
+  ]);
+};
 
-function buildMenusIndexes() {
-  r.table('menus').indexCreate('name').run();
-  r.table('menus').indexCreate('location').run();
-  r.table('menus').indexCreate('links').run();
-}
-
-function initSettings() {
-  r.tableCreate('settings').run()
-  .then(() => buildSettingsIndexes());
-}
-
-function buildSettingsIndexes() {
-  r.table('settings').indexCreate('name').run();
-  r.table('settings').indexCreate('global').run();
-}
-
-function initPages() {
-  r.tableCreate('pages').run()
-  .then(() => buildPagesIndexes());
-}
-
-function buildPagesIndexes() {
-  r.table('pages').indexCreate('name').run();
-  r.table('pages').indexCreate('slug').run();
-  r.table('pages').indexCreate('visibility').run();
-}
-
-function initCollections() {
-  r.tableCreate('collections').run()
-  .then(() => buildCollectionsIndexes());
-}
-
-function buildCollectionsIndexes() {
-  r.table('collections').indexCreate('name').run();
-  r.table('collections').indexCreate('slug').run();
-  r.table('collections').indexCreate('authorId').run();
-  r.table('collections').indexCreate('mediaType').run();
-}
-
-initDB();
-initUser();
-initArticle();
-initTag();
-initArticleTags();
-initMenus();
-initSettings();
-initPages();
+setupDB();
