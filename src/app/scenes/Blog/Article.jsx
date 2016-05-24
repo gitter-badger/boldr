686778f9f9498d049670cbd1c336c9b831dd165c
Steven Truesdell <steven@strues.io>
2016-05-23 23:16:12 -0600

    Working article display (#20)
    * Displaying rich text from article editor on blog page.
    
    * cleanup the article comp

--- src/app/scenes/Blog/Article.jsx
+++ src/app/scenes/Blog/Article.jsx
@@ -1,80 +1,78 @@
 import React from 'react';
+import { articleTypes } from 'app/components/Editor/utilities';
 import { Editor, EditorState } from 'draft-js';
-// import { createEditorStateFromRawDraft } from 'app/components/Editor/helpers/convertEditorState';.
+import Display from 'app/components/Editor/Display';
+import { createEditorStateFromRawDraft } from 'app/components/Editor/helpers/convertEditorState';
 import _ from 'lodash';
-// import { articleTypes } from 'app/components/Editor/utilities';
 
 class Article extends React.Component {
 
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   editorState: createEditorStateFromRawDraft(this.prepareDraft(props.rawDraft))
-    // };
-    // this.getChildContext = () => {
-    //   return {
-    //     articleState: articleTypes.FULL,
-    //     articleUrl: this.props.articleUrl
-    //   }
-    // }
+    this.state = {
+      editorState: createEditorStateFromRawDraft(this.prepareDraft(props.content))
+    };
+
+    this.getChildContext = () => {
+      return {
+        articleState: articleTypes.FULL,
+        slug: this.props.slug
+      };
+    };
   }
 
-  /** OVERWRITTEN IN ArticleOverview **/
-  // prepareDraft(draft) {
-  //   return draft;
-  // }
-  //
-  // componentWillReceiveProps(newProps) {
-  //   if (newProps.rawDraft !== this.props.rawDraft) {
-  //     this.setState({
-  //       editorState: createEditorStateFromRawDraft(
-  //         this.prepareDraft(newProps.rawDraft)
-  //       )
-  //     });
-  //   }
-  //
-  //
-  // }
+  componentWillReceiveProps(newProps) {
+    if (newProps.content !== this.props.content) {
+      this.setState({
+        editorState: createEditorStateFromRawDraft(
+          this.prepareDraft(newProps.content)
+        )
+      });
+    }
+  }
+
+  prepareDraft(draft) {
+    return draft;
+  }
 
   render() {
-    const {editorState} = this.state;
+    const { editorState } = this.state;
 
-    const {blockStyleFn} = this.props;
-    const {blockRendererFn} = this.props;
-    const {customStyleMap} = this.props;
+    const { customStyleMap, content, title, slug } = this.props;
 
     let className = 'card RichEditor-content';
 
     let titleHeader;
     if (this.props.title) {
-      titleHeader = <h4 className="header"> {this.props.title} </h4>
+      titleHeader = <h4 className="header"> { this.props.title } </h4>;
     }
 
     return (
       <div>
-                {titleHeader}
-                <div className={className}>
-                    <div className="card-content">
-
-                    </div>
-                </div>
+        { titleHeader }
+          <div className={ className }>
+            <div className="card-content">
+              <Editor readOnly
+                customStyleMap={ customStyleMap }
+                editorState={ editorState }
+              />
             </div>
+          </div>
+        </div>
       );
   }
 }
 
 Article.childContextTypes = {
   articleState: React.PropTypes.string,
-  articleUrl: React.PropTypes.string
+  slug: React.PropTypes.string
 };
 
 Article.propTypes = {
-  blockStyleFn: React.PropTypes.func.isRequired,
-  blockRendererFn: React.PropTypes.func.isRequired,
-  customStyleMap: React.PropTypes.object.isRequired,
-  rawDraft: React.PropTypes.object.isRequired,
-  articleUrl: React.PropTypes.string.isRequired,
+  content: React.PropTypes.object.isRequired,
+  customStyleMap: React.PropTypes.object,
+  slug: React.PropTypes.string.isRequired,
   title: React.PropTypes.string
 };
 
--- src/app/scenes/Blog/BlogContainer.jsx
+++ src/app/scenes/Blog/BlogContainer.jsx
@@ -2,49 +2,43 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchArticles } from 'app/state/article/article.actions';
 import Loader from '../../components/Loader';
-// import Article from './components/Article';
+import Article from './Article';
 
 class BlogContainer extends Component {
-  constructor(props) {
-    super(props);
-    // this.createArticleList = (articleList) => this._createArticleList(articleList);
-  }
   static loadAsyncData(dispatch) {
     return dispatch(fetchArticles());
+  }
+
+  constructor(props) {
+    super(props);
+    this.createArticleList = (articleList) => this._createArticleList(articleList);
   }
 
   componentDidMount() {
     this.constructor.loadAsyncData(this.props.dispatch);
   }
-  //
-  // _createArticleList(articles) {
-  //   let articleList = [];
-  //   for (var article of articles) {
-  //     articleList.push(
-  //       <div key={article.articleId}>
-  //               <Article
-  //       blockStyleFn={this.blockStyleFn}
-  //       blockRendererFn={this.blockRendererFn}
-  //       customStyleMap={this.customStyleMap}
-  //       rawDraft={article.content}
-  //       articleUrl={article.slug}
-  //       />
-  //               <div className="section"/>
-  //           </div>
-  //     )
-  //   }
-  //   return articleList;
-  // }
+
+  _createArticleList(articles) {
+    const articleList = [];
+    for (let article of articles) { // eslint-disable-line
+      articleList.push(
+        <div key={ article.articleId }>
+          <Article {...article} />
+        </div>
+      );
+    }
+    return articleList;
+  }
   render() {
-    const {loading, article} = this.props;
-    // let articleList = this.createArticleList(this.props.article.articles);
+    const { loading, article } = this.props;
+    let articleList = this.createArticleList(this.props.article.articles);
 
     return (
       <div>
 
        <div className="container">
          BlogContainer?
-
+         { articleList }
        </div>
       </div>
       );
@@ -53,7 +47,8 @@ class BlogContainer extends Component {
 
 BlogContainer.propTypes = {
   loading: PropTypes.bool,
-  article: PropTypes.object
+  article: PropTypes.object,
+  dispatch: PropTypes.func
 };
 
 const mapStateToProps = (state) => ({
