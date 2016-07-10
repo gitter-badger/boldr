4514d49e11ab8997ea283e7d0d027fbba975df3f
Steven Truesdell <steven@strues.io>
2016-07-10 14:18:05 -0600

    fix onchange warning in editor

--- src/shared/scenes/Dashboard/components/pg.ArticleEditor/ArticleEditor.jsx
+++ src/shared/scenes/Dashboard/components/pg.ArticleEditor/ArticleEditor.jsx
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
-
+import { EditorState } from 'draft-js';
 import BoldrEditor from 'shared/components/org.BoldrEditor';
 
 class ArticleEditor extends Component {
   constructor(props) {
     super(props);
-
-    this.onChange = (value) => {
-      this.setState({
-        value
-      });
+    this.state = {
+      editorState: EditorState.createEmpty()
     };
+    // this.onChange = (value) => {
+    //   this.setState({
+    //     value
+    //   });
+    // };
 
     this.getMarkup = (markup) => {
       this.setState({
         markup
       });
     };
-
+    this.onChange = (editorState) => this.setState({ editorState });
     this.renderInnerMarkup = () => this._renderInnerMarkup();
     this.renderReturnedContent = (value) => this._renderReturnedContent(value);
-
-    this.state = {
-    };
   }
   render() {
     return (
       <div>
         Editor
-        <BoldrEditor />
+        <BoldrEditor editorState={ this.state.editorState }
+          onChange={ this.onChange }
+        />
       </div>
     );
   }
